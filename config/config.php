c6d3e4f7e67b6fa71d36155a6314cffa5c30aa99
AlexCnk97 <alexchinke97@gmail.com>
2020-05-25 18:22:14 -0600

    api rest

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('URL', 'http://localhost/arteycultura/');
+
+define('HOST', 'localhost');
+define('DB', 'arteycultura');
+define('USER', 'developer');
+define('PASSWORD', "admin");
+define('CHARSET', 'utf8mb4');
+
+?>
